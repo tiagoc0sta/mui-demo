ad1a6ef16ca3316121a1eebc0882c6b3b3e76d4e
Tiago Costa <tiagodpv@gmail.com>
2023-02-12 04:39:46 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toollbar,
+} from '@mui/material';
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant='h1'>Hello, World!</Typography>
+    </div>
+  );
+};
+
+export default App;
